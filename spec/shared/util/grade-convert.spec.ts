50cacb9b1b7a949fee79375a2e15e8743f9d0828
Michael Ingram <mpingram92@gmail.com>
2018-02-16 12:09:01 -0600

    Refactor spec/shared/util/grade-util
    * Rewrite tests to adapt to changed api -- use toLetterGrade
      method instead of toString

--- spec/shared/util/grade-convert.spec.ts
+++ spec/shared/util/grade-convert.spec.ts
@@ -1,6 +1,6 @@
 import {expect} from "chai";
 import {
-  scoreToString,
+  toLetterGrade,
   GradeConvertErrors
 } from "../../../src/shared/util/grade-convert";
 import {
@@ -11,48 +11,48 @@ import {
 import ScoreType from "../../../src/shared/enums/score-type";
 
 
-describe("scoreToString", () => {
+describe("toLetterGrade", () => {
 
   it("should convert A numeric grades to 'A'", () => {
     const score = 91;
     const scoreType = ScoreType.subjGradeMath;
-    expect(scoreToString(score, scoreType)).to.equal("A");
+    expect(toLetterGrade(score)).to.equal("A");
   });
 
   it("should convert B numeric grades to 'B'", () => {
     const score = 81;
     const scoreType = ScoreType.subjGradeMath;
-    expect(scoreToString(score, scoreType)).to.equal("B");
+    expect(toLetterGrade(score)).to.equal("B");
   });
 
   it("should convert C numeric grades to 'C'", () => {
     const score = 71;
     const scoreType = ScoreType.subjGradeMath;
-    expect(scoreToString(score, scoreType)).to.equal("C");
+    expect(toLetterGrade(score)).to.equal("C");
   });
 
   it("should convert D numeric grades to 'D'", () => {
     const score = 61;
     const scoreType = ScoreType.subjGradeMath;
-    expect(scoreToString(score, scoreType)).to.equal("D");
+    expect(toLetterGrade(score)).to.equal("D");
   });
 
   it("should convert F numeric grades to 'F'", () => {
     const score = 51;
     const scoreType = ScoreType.subjGradeMath;
-    expect(scoreToString(score, scoreType)).to.equal("F");
+    expect(toLetterGrade(score)).to.equal("F");
   });
 
   it("should convert numeric grades of value 100 to correct letter grade", () => {
     const score = 100;
     const scoreType = ScoreType.subjGradeRead;
-    expect(scoreToString(score, scoreType)).to.equal("A");
+    expect(toLetterGrade(score)).to.equal("A");
   });
 
   it("should convert numeric grades of value zero to correct letter grade", () => {
     const score = 0;
     const scoreType = ScoreType.subjGradeRead;
-    expect(scoreToString(score, scoreType)).to.equal("F");
+    expect(toLetterGrade(score)).to.equal("F");
   });
 
 });
