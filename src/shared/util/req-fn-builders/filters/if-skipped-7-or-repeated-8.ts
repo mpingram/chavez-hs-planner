80a2b86e9c8868bec125febf01261abc4a39c44b
Michael Ingram <mpingram92@gmail.com>
2018-03-13 17:46:13 -0500

    Fix logic error in ifSkipped7OrRepeated8 req fn filter
    * NOTE I actually completely swapped the true/false values for
      ifSkipped7OrRepeated8, the fn was returning true when we didn't
      want it to.

--- src/shared/util/req-fn-builders/filters/if-skipped-7-or-repeated-8.ts
+++ src/shared/util/req-fn-builders/filters/if-skipped-7-or-repeated-8.ts
@@ -2,8 +2,8 @@ import HSReqFilter from "shared/types/hs-req-filter";
 
 export const ifSkipped7OrRepeated8: HSReqFilter = (student, program) => {
   if (student.gradeLevel === 8 && student.prevGradeLevel !== 7) {
-    return false;
-  } else {
     return true;
+  } else {
+    return false;
   }
 };
