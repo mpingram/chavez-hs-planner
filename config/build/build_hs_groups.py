6ce4d9825dec77951672447b27bc1f178b2c988f
Michael Ingram <mpingram92@gmail.com>
2018-03-21 14:33:15 -0500

    Debug hs groups build script

--- config/build/build_hs_groups.py
+++ config/build/build_hs_groups.py
@@ -1,4 +1,5 @@
 from os import path
+import logging
 import json
 import sys
 
@@ -27,16 +28,21 @@ def get_ids_of(ptype, **kwargs):
     programs = kwargs['programs']
     output = []
     for program in programs:
-        if program['Program_Type'] == ptype:
+        is_hs_program = program['Primary_Category'] == 'HS'
+        matches_program_type = program['Program_Type'] == ptype
+
+        if is_hs_program and matches_program_type:
             output.append(program['ID'])
 
-def make_hs_program_groups(config_f, cps_programs_f, output_f):
-    hs_group_config = json.load(input_f)
-    cps_programs = json.load(cps_programs_f)
+    return output
+
+def make_hs_program_groups(config, cps_programs):
 
     output = []
 
-    for group_spec in hs_group_config:
+    prev_used_program_types = []
+
+    for group_spec in config:
         groupName = group_spec['groupName']
         groupDesc = group_spec['groupDesc']
         programTypes = group_spec['includes']['programTypes']
@@ -44,6 +50,17 @@ def make_hs_program_groups(config_f, cps_programs_f, output_f):
         programIDs = [] 
         for programType in programTypes:
             ids = get_ids_of(programType, programs=cps_programs)
+
+            # warn user if no ids found
+            if len(ids) == 0:
+                logging.warning('Group {0}: Program Type {1} failed to match any programs'.format(groupName, programType))
+
+            # warn user if they try to use the same program type twice
+            if programType in prev_used_program_types:
+                logging.warning('Group {0}: Program Type {1} was listed multiple times'.format(groupName, programType))
+
+            prev_used_program_types.append(programType)
+
             programIDs.extend(ids)
 
         output.append({
@@ -52,7 +69,7 @@ def make_hs_program_groups(config_f, cps_programs_f, output_f):
             'programIDs': programIDs
             })
 
-    return json.dump(output, output_f)
+    return output
 
 if __name__ == "__main__":
     path_to_config = sys.argv[1]
@@ -61,6 +78,11 @@ if __name__ == "__main__":
 
     with open(path_to_conf, 'r') as conf_f:
         with open(path_to_cps_program, 'r') as cps_programs_f:
+
+            hs_group_config = json.load(input_f)
+            cps_programs = json.load(cps_programs_f)
+            groups = make_hs_program_groups(hs_group_config, cps_programs)
+
             with open(path_to_output, 'w') as out_f:
-                make_hs_program_groups(conf_f, cps_programs_f,  outf)
+                json.dump(groups, out_f)
 
