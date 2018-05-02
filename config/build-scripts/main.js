c8b39c25f0530175c6f1cd97924313910208adf4
Michael Ingram <mpingram92@gmail.com>
2018-05-02 12:09:53 -0500

    Move cutoff scores to config/data
    * DONE come up with a consistent schema for cutoff scores.
      ** NOTE the most consistent thing may be to separate SE
      cutoff scores from non-SE cutoff scores, under the assumption
      that no other school will do the tier thing.
    * add historical cutoff scores json to config/data
    * add historical cutoff scores json schema to config/schema
    * write cutoff scores processing script
      ** validate schema in processing script
    
    * NOTE IMPORTANT cutoff scores are NOT currently linked to
      programs via program IDs.
      ** The reason is that I'm considering a change to the program ID
      approach that will be less likely to change over time. Currently,
      programIDs are created by hashing two properties of the raw program
      data -- the school name and the program type. This means that small
      changes in the formatting of school names and program types
      (like capitalization changes, etc) result in completely different
      programIDs, even though they're describing the 'same' program.
      ** What I'm considering instead is to make the programID of the
      form <schoolID>-<programTypeID>, where schoolID is the standard
      CPS school ID and programTypeID is an ID the app comes up with by
      normalizing program types.

--- config/build-scripts/main.js
+++ config/build-scripts/main.js
@@ -7,6 +7,7 @@ const rawProgramDataSchema = require("../schema/raw-data/raw-program-data");
 const rawAttendanceBoundariesSchema = require("../schema/raw-data/raw-attendance-boundaries.js");
 const tractTierTableConfigSchema = require("../schema/raw-data/tract-tier-table-config.js");
 const programGroupsConfigSchema = require("../schema/raw-data/program-groups-config.js");
+const cutoffScoresSchema = require("../schema/raw-data/cutoff-scores.json");
 
 // CONSIDER IN FUTURE add date to config files
 
@@ -27,6 +28,7 @@ const INPUT_FILEPATH_RAW_PROGRAM_DATA = path.join(srcDir, "program-data", "Chica
 const INPUT_FILEPATH_RAW_HS_ATTENDANCE_BOUND_GEOMETRY = path.join(srcDir, "hs-attendance-boundaries", "Chicago Public Schools - High School Attendance Boundaries SY1718.geojson");
 const INPUT_FILEPATH_PROGRAM_GROUPS = path.join(srcDir, "program-groups", "program-groups.config.json");
 const INPUT_FILEPATH_TRACT_TIER_TABLE = path.join(srcDir, "tract-tier-table", "tract-tier-table.json");
+const INPUT_FILEPATH_CUTOFF_SCORES = path.join(srcDir, "cutoff-scores", "cutoff-scores.json");
 
 // Processed data filepaths
 // ------------------
@@ -35,6 +37,7 @@ const OUTPUT_FILEPATH_PROGRAM_DATA = path.join(destDir, hs-program-data.json);
 const OUTPUT_FILEPATH_ATTENDANCE_BOUND_GEOMETRIES = path.join(destDir, "school-attendance-boundary-table.json");
 const OUTPUT_FILEPATH_PROGRAM_GROUPS = path.join(destDir, "program-groups.json");
 const OUTPUT_FILEPATH_TRACT_TIER_TABLE = path.join(destDir, "tract-tier-table.json");
+const OUTPUT_FILEPATH_CUTOFF_SCORES = path.join(destDir, "cutoff-scores.json");
 
 // ==================
 
@@ -43,9 +46,7 @@ function buildAll() {
   // --
   const rawProgramDataCsv = fs.readFileSync(INPUT_FILEPATH_RAW_PROGRAM_DATA, "utf-8");
   const rawProgramDataJSON = json2csv.parse(rawProgramData);
-
   validateOrThrow(rawProgramDataJSON, rawProgramDataSchema);
-
   const validationResult = jsonschema.validate(rawProgramDataJSON, rawProgramDataSchema);
   const programData = buildProgramData(rawProgramData);
   fs.writeFileSync(OUTPUT_FILEPATH_PROGRAM_DATA, programData, "utf-8");
@@ -53,9 +54,7 @@ function buildAll() {
   // School attendance boundaries
   // --
   const rawHSAttendanceBoundGeojson = JSON.parse(fs.readFileSync(INPUT_FILEPATH_RAW_HS_ATTENDANCE_BOUND_GEOMETRY, "utf-8"));
-
   validateOrThrow(rawHSAttendanceBoundGeojson, rawAttendanceBoundariesSchema);
-
   const schoolAttendanceBoundTable = buildSchoolAttendanceBoundaryTable(rawHSAttendanceBoundGeojson, ATTENDANCE_BOUND_COORDINATE_PRECISION);
   fs.writeFileSync(OUTPUT_FILEPATH_ATTENDANCE_BOUND_GEOMETRIES, schoolAttendanceBoundTable, "utf-8");
 
@@ -68,8 +67,15 @@ function buildAll() {
   // Tract-tier table
   // --
   const tractTierTableJSON = JSON.parse(fs.readFileSync(INPUT_FILEPATH_TRACT_TIER_TABLE, "utf-8"));
-  validateOrThrow(tractTierTableJSON, tractTierTableConfig);
+  validateOrThrow(tractTierTableJSON, tractTierTableConfigSchema);
   fs.copyFileSync(INPUT_FILEPATH_TRACT_TIER_TABLE, OUTPUT_FILEPATH_TRACT_TIER_TABLE);
+
+  // Historical cutoff scores
+  // --
+  const cutoffScoresJSON = JSON.parse(fs.readFileSync(INPUT_FILEPATH_CUTOFF_SCORES, "utf-8"));
+  validateOrThrow(cutoffScoresJSON, cutoffScoresSchema);
+  fs.copyFileSync(INPUT_FILEPATH_TRACT_TIER_TABLE, OUTPUT_FILEPATH_TRACT_TIER_TABLE);
+
 }
 
 function validateOrThrow(json, schema) {
