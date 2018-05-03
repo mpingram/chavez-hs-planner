98b95c2e255eb80b223e6df8a88292d8a2510cd9
Michael Ingram <mpingram92@gmail.com>
2018-05-03 14:40:12 -0500

    Clean up data in config/data
    * fix misformatted cutoff scores
    * delete program groups -- can come back to this
      commit to restore them.

--- config/build-scripts/main.js
+++ config/build-scripts/main.js
@@ -9,10 +9,8 @@ const buildSchoolAttendanceBoundaryTable = require("./build-school-attendance-bo
 const rawProgramDataSchema = require("../schema/raw-data/raw-program-data");
 const rawAttendanceBoundariesSchema = require("../schema/raw-data/raw-attendance-boundaries.js");
 const tractTierTableConfigSchema = require("../schema/raw-data/tract-tier-table-config.js");
-const programGroupsConfigSchema = require("../schema/raw-data/program-groups-config.js");
 const cutoffScoresSchema = require("../schema/raw-data/cutoff-scores.json");
 const programTypeIDsConfigSchema = require("../schema/raw-data/program-type-ids.config.json");
-
 
 // Constants
 // ==================
@@ -28,7 +26,6 @@ const srcDir = path.resolve(__dirname, "..", "data");
 const INPUT_FILEPATH_RAW_PROGRAM_DATA = path.join(srcDir, "program-data", "Chicago_Public_Schools_-_School_Admissions_Information_SY1617.csv");
 //const INPUT_FILEPATH_RAW_ES_ATTENDANCE_BOUND_GEOMETRY = path.join(srcDir, "es-attendance-boundaries", "Chicago Public Schools - Elementary School Attendance Boundaries SY1718.geojson");
 const INPUT_FILEPATH_RAW_HS_ATTENDANCE_BOUND_GEOMETRY = path.join(srcDir, "hs-attendance-boundaries", "Chicago Public Schools - High School Attendance Boundaries SY1718.geojson");
-const INPUT_FILEPATH_PROGRAM_GROUPS = path.join(srcDir, "program-groups", "program-groups.config.json");
 const INPUT_FILEPATH_TRACT_TIER_TABLE = path.join(srcDir, "tract-tier-table", "tract-tier-table.json");
 const INPUT_FILEPATH_CUTOFF_SCORES = path.join(srcDir, "cutoff-scores", "cutoff-scores.json");
 const INPUT_FILEPATH_PROGRAM_TYPES_CONFIG = path.join(srcDir, "program-type-ids", "program-type-ids.config.json");
@@ -38,7 +35,7 @@ const INPUT_FILEPATH_PROGRAM_TYPES_CONFIG = path.join(srcDir, "program-type-ids"
 const destDir = path.resolve(__dirname, "..", "..", "dist", "data");
 const OUTPUT_FILEPATH_PROGRAM_DATA = path.join(destDir, hs-program-data.json);
 const OUTPUT_FILEPATH_ATTENDANCE_BOUND_GEOMETRIES = path.join(destDir, "school-attendance-boundary-table.json");
-const OUTPUT_FILEPATH_PROGRAM_GROUPS = path.join(destDir, "program-groups.json");
+//const OUTPUT_FILEPATH_PROGRAM_GROUPS = path.join(destDir, "program-groups.json");
 const OUTPUT_FILEPATH_TRACT_TIER_TABLE = path.join(destDir, "tract-tier-table.json");
 const OUTPUT_FILEPATH_CUTOFF_SCORES = path.join(destDir, "cutoff-scores.json");
 const OUTPUT_FILEPATH_PROGRAM_TYPE_ID_TABLE = path.join(destDir, "program-type-id-table.json");
@@ -54,12 +51,6 @@ function buildAll() {
   const schoolAttendanceBoundTable = buildSchoolAttendanceBoundaryTable(rawHSAttendanceBoundGeojson, ATTENDANCE_BOUND_COORDINATE_PRECISION);
   const schoolAttendanceBoundTableJSON = JSON.stringify(schoolAttendanceBoundTable);
   fs.writeFileSync(OUTPUT_FILEPATH_ATTENDANCE_BOUND_GEOMETRIES, schoolAttendanceBoundTableJSON, "utf-8");
-
-  // Program Groups
-  // --
-  const programGroups = JSON.parse(fs.readFileSync(INPUT_FILEPATH_PROGRAM_GROUPS, "utf-8"));
-  validateOrThrow(programGroups, programGroupsConfigSchema);
-  fs.copyFileSync(INPUT_FILEPATH_PROGRAM_GROUPS, OUTPUT_FILEPATH_PROGRAM_GROUPS);
 
   // Tract-tier table
   // --
