03b21955b7ed0c28e15b25b8d1a03485f540bc91
Michael Ingram <mpingram92@gmail.com>
2018-07-10 13:21:06 -0500

    Remove program type IDs, extra folders from config data
    * NOTE Having to work with program type ID system again when updating
      data, I decided that it was too complex and failure-prone for the
      limited benefit (correcting the spellings of program types),
      especially if we're going to be grouping the programs by school in the
      future.
    
    * Added a spelling correcter to build-program-data build script

--- config/build-scripts/main.js
+++ config/build-scripts/main.js
@@ -4,19 +4,16 @@ const csvParseSync = require("csv-parse/lib/sync");
 const jsonschema = require("jsonschema");
 
 const createProgramData = require("./create-program-data");
-const createProgramTypeIDTable = require("./create-program-type-id-table");
 const createSchoolAttendanceBoundaryTable = require("./create-school-attendance-boundary-table");
 
 const schemaDir = path.resolve(__dirname, "..", "schema");
-const rawProgramDataSchema = require(path.resolve(schemaDir, "raw-program-data.json"));
+const rawProgramDataSchema = require(path.resolve(schemaDir, "program-data.json"));
 const rawAttendanceBoundariesSchema = require(path.resolve(schemaDir, "raw-attendance-boundaries.json"));
 const tractTierTableConfigSchema = require(path.resolve(schemaDir, "tract-tier-table.json"));
 const seCutoffScoresSchema = require(path.resolve(schemaDir, "se-cutoff-scores.json"));
 const nonSECutoffScoresSchema = require(path.resolve(schemaDir, "non-se-cutoff-scores.json"));
-const programTypeIDsConfigSchema = require(path.resolve(schemaDir, "program-type-ids.config.json"));
 
 const schoolAttendanceBoundTableSchema = require(path.resolve(schemaDir, "school-attendance-boundary-table.json"));
-const programTypeIDTableSchema = require(path.resolve(schemaDir, "program-type-id-table.json"));
 const hsProgramsSchema = require(path.resolve(schemaDir, "hs-programs.json"));
 const nonHSProgramsSchema = require(path.resolve(schemaDir, "non-hs-programs.json"));
 
@@ -58,15 +55,13 @@ if (!mostRecentVersion) {
   throw new Error(`No versioned data folders found in ${rawDataParentDir}.\nSubfolders of this directory should be named in ISO8601 date format, ie 2017-01-01.`);
 }
 const srcDir = path.resolve(rawDataParentDir, mostRecentVersion);
-console.log(srcDir);
 
-const INPUT_FILEPATH_RAW_PROGRAM_DATA = path.join(srcDir, "raw-program-data", "Chicago_Public_Schools_-_School_Admissions_Information_SY1617.csv");
-//const INPUT_FILEPATH_RAW_ES_ATTENDANCE_BOUND_GEOMETRY = path.join(srcDir, "es-attendance-boundaries", "Chicago Public Schools - Elementary School Attendance Boundaries SY1718.geojson");
-const INPUT_FILEPATH_RAW_HS_ATTENDANCE_BOUND_GEOMETRY = path.join(srcDir, "hs-attendance-boundaries", "Chicago Public Schools - High School Attendance Boundaries SY1718.geojson");
-const INPUT_FILEPATH_TRACT_TIER_TABLE = path.join(srcDir, "tract-tier-table", "tract-tier-table.json");
-const INPUT_FILEPATH_SE_CUTOFF_SCORES = path.join(srcDir, "se-cutoff-scores", "se-cutoff-scores.json");
-const INPUT_FILEPATH_NON_SE_CUTOFF_SCORES = path.join(srcDir, "non-se-cutoff-scores", "non-se-cutoff-scores.json");
-const INPUT_FILEPATH_PROGRAM_TYPE_IDS = path.join(srcDir, "program-type-ids", "program-type-ids.config.json");
+const INPUT_FILEPATH_RAW_PROGRAM_DATA = path.join(srcDir, "program-data.csv");
+//const INPUT_FILEPATH_RAW_ES_ATTENDANCE_BOUND_GEOMETRY = path.join(srcDir, "es-attendance-boundaries.geojson");
+const INPUT_FILEPATH_RAW_HS_ATTENDANCE_BOUND_GEOMETRY = path.join(srcDir, "hs-attendance-boundaries.geojson");
+const INPUT_FILEPATH_TRACT_TIER_TABLE = path.join(srcDir, "tract-tier-table.json");
+const INPUT_FILEPATH_SE_CUTOFF_SCORES = path.join(srcDir, "se-cutoff-scores.json");
+const INPUT_FILEPATH_NON_SE_CUTOFF_SCORES = path.join(srcDir, "non-se-cutoff-scores.json");
 
 // Processed data filepaths
 // ------------------
@@ -76,7 +71,6 @@ const OUTPUT_FILEPATH_ATTENDANCE_BOUND_GEOMETRIES = path.join(destDir, "school-a
 const OUTPUT_FILEPATH_TRACT_TIER_TABLE = path.join(destDir, "tract-tier-table.json");
 const OUTPUT_FILEPATH_SE_CUTOFF_SCORES = path.join(destDir, "se-cutoff-scores.json");
 const OUTPUT_FILEPATH_NON_SE_CUTOFF_SCORES = path.join(destDir, "non-se-cutoff-scores.json");
-const OUTPUT_FILEPATH_PROGRAM_TYPE_ID_TABLE = path.join(destDir, "program-type-id-table.json");
 const OUTPUT_FILEPATH_HS_PROGRAMS = path.join(destDir, "hs-programs.json");
 const OUTPUT_FILEPATH_NON_HS_PROGRAMS = path.join(destDir, "non-hs-programs.json");
 
@@ -87,7 +81,6 @@ function buildAll() {
   buildSchoolAttendanceBoundaryTable();
   buildTractTierTable();
   buildCutoffScores();
-  buildProgramTypeIDTable();
   buildProgramData();
 
 }
@@ -140,47 +133,14 @@ function buildCutoffScores() {
   fs.writeFileSync(OUTPUT_FILEPATH_NON_SE_CUTOFF_SCORES, JSON.stringify(nonSECutoffScores), "utf-8");
 }
 
-function buildProgramTypeIDTable() {
-  const programTypeIDsConfig = JSON.parse(fs.readFileSync(INPUT_FILEPATH_PROGRAM_TYPE_IDS, "utf-8"));
-  validateOrThrow(programTypeIDsConfig, programTypeIDsConfigSchema);
-
-  // test each program type id for uniqueness
-  let keys = {};
-  let duplicateKey;
-  const hasUniqueKeys = programTypeIDsConfig.every( record => {
-    const id = record.programTypeID;
-    let isUnique;
-    if (keys[id] === undefined) {
-      isUnique = true;
-    } else {
-      duplicateKey = id;
-      isUnique = false;
-    }
-    // add key to hashtable of keys we've already seen
-    keys[id] = true;
-    return isUnique;
-  });
-  if (!hasUniqueKeys) {
-    throw new Error(`Two or more records in the program type id config share the same programTypeID property: ${duplicateKey}. All programTypeID properties in the program type id config must be unique.`);
-  }
-
-  const programTypeIDTable = createProgramTypeIDTable(programTypeIDsConfig);
-  validateOrThrow(programTypeIDTable, programTypeIDTableSchema);
-
-  fs.writeFileSync(OUTPUT_FILEPATH_PROGRAM_TYPE_ID_TABLE, JSON.stringify(programTypeIDTable), "utf-8");
-}
-
 function buildProgramData() {
-  const programTypeIDsConfig = JSON.parse(fs.readFileSync(INPUT_FILEPATH_PROGRAM_TYPE_IDS, "utf-8"));
-  validateOrThrow(programTypeIDsConfig, programTypeIDsConfigSchema);
-
   const rawProgramDataCsv = fs.readFileSync(INPUT_FILEPATH_RAW_PROGRAM_DATA, "utf-8");  
   // parse csv file into js object
   const rawProgramData = csvParseSync(rawProgramDataCsv, {columns: true});
   validateOrThrow(rawProgramData, rawProgramDataSchema);
   let programData;
   try {
-    programData = createProgramData(rawProgramData, programTypeIDsConfig);
+    programData = createProgramData(rawProgramData);
   } catch(e) {
     throw(e);
   }
@@ -212,4 +172,7 @@ function validateOrThrow(json, schema) {
   }
 }
 
+/* 
+ * Run all build scripts.
+ * */
 buildAll();
